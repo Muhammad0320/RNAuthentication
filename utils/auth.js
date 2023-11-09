f114d7aa7e2288b7b1f4079798478737682368fb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-09 04:55:53 +0100

    feat: addd reuasable function; authenticate for the auth process

--- utils/auth.js
+++ utils/auth.js
@@ -1,1 +1,14 @@
-const authenticate = () => {};
+import axios from "axios";
+import { API_KEY, BASE_URL } from "./constants";
+
+const authenticate = async (mode, email, password) => {
+  const url = `${BASE_URL}${mode}?key=${API_KEY}`;
+
+  const res = await axios.post(url, {
+    email,
+    password,
+    returnSecureToken: true,
+  });
+
+  return res.data;
+};
