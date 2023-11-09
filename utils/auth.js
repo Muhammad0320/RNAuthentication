703b625fdcd636e6805955c6dd579b513b83ac17
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-09 06:22:09 +0100

    chore: fix the authentication

--- utils/auth.js
+++ utils/auth.js
@@ -4,6 +4,8 @@ import { API_KEY, BASE_URL } from "./constants";
 const authenticate = async (mode, email, password) => {
   const url = `${BASE_URL}${mode}?key=${API_KEY}`;
 
+  // const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
+
   const res = await axios.post(url, {
     email,
     password,
