10b030736e7a2c24ad548ce1fa1b7efc2cf098f5
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-09 06:45:17 +0100

    chore: add authentication to signuo screen

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -2,13 +2,22 @@ import { useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
 import { signupApi } from "../utils/auth";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { Alert } from "react-native";
 
 function SignupScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
   const handleSignup = async ({ email, password }) => {
     setIsAuthenticating(true);
-    await signupApi(email, password);
+
+    try {
+      await signupApi(email, password);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed",
+        "Could not create account, please check your input and try again"
+      );
+    }
 
     setIsAuthenticating(false);
   };
