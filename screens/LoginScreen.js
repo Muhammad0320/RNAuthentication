5517dba7461d736a336cb30817d81ddc8ae60c70
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-09 06:43:01 +0100

    chore: add error handling tologin screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,13 +2,21 @@ import { useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
 import { loginApi } from "../utils/auth";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { Alert } from "react-native";
 
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
   const handleLogin = async ({ email, password }) => {
     setIsAuthenticating(true);
 
-    await loginApi(email, password);
+    try {
+      await loginApi(email, password);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed",
+        "Could not log you in, please check your login credentials and try again"
+      );
+    }
 
     setIsAuthenticating(false);
   };
