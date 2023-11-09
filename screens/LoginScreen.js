3a75e3502b463bbc14cdfa0a709ff22d7c21c6a4
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-11-09 06:33:29 +0100

    chore: add loading spiiner to login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
 import { loginApi } from "../utils/auth";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
 
 function LoginScreen() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
   const handleLogin = async ({ email, password }) => {
+    setIsAuthenticating(true);
+
     await loginApi(email, password);
+
+    setIsAuthenticating(false);
   };
+
+  if (isAuthenticating) return <LoadingOverlay message="Logging you in..." />;
 
   return <AuthContent isLogin onAuthenticate={handleLogin} />;
 }
